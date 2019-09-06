650fa2dc5bf819ed721723cf115506125cd9e640
Ubuntu <ubuntu@puppet.dev.us-east-1.graduway.com>
2019-09-06 11:29:49 +0000

    add permissions to node_modules global folder

--- site/profile/manifests/nodejs.pp
+++ site/profile/manifests/nodejs.pp
@@ -5,6 +5,12 @@ class profile::nodejs (
 {
   class { 'nodejs':
     repo_url_suffix => $version,
+    nodejs_dev_package_ensure => 'present',
+    npm_package_ensure        => 'present',
+  }
+  file {'/usr/lib/node_modules':
+    ensure => directory,
+    mode   => '777',
   }
   $npm_packages.each |String $npm_package| {
     package { $npm_package:
